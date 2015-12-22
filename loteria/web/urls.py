7b501a22318a10a0a87a15547364d40f1f53ac6b
mUniKeS <munikes@members.fsf.org>
2015-12-22 22:50:56 +0100

    index.html without login

--- loteria/web/urls.py
+++ loteria/web/urls.py
@@ -5,10 +5,11 @@ from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
-            url(r'^list/$', login_required(views.LotteryUserList.as_view()), name='lotteryuser-list'),
+            #url(r'^list/$', login_required(views.LotteryUserList.as_view()), name='lotteryuser-list'),
+            url(r'^$', views.LotteryUserList.as_view(), name='lotteryuser-list'),
             url(r'^create/$', login_required(views.LotteryUserCreate.as_view()), name='lotteryuser-create'),
             url(r'^update/(?P<pk>\d+)$', login_required(views.LotteryUserUpdate.as_view()), name='lotteryuser-update'),
             url(r'^delete/(?P<pk>\d+)$', login_required(views.LotteryUserDelete.as_view()), name='lotteryuser-delete'),
             url(r'^logout/$', auth_views.logout, name="logout"),
-            url(r'^$', auth_views.login, name="login"),
+            #url(r'^$', auth_views.login, name="login"),
             ]
